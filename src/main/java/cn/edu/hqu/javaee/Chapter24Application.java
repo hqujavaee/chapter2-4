fb01461258fcf549898271c5509a9be2f2013555
27597581 <27597581@qq.com>
2018-04-12 18:51:32 +0800

    第一次提交

---
+++ src/main/java/cn/edu/hqu/javaee/Chapter24Application.java
@@ -0,0 +1,10 @@
+package cn.edu.hqu.javaee;
+
+import org.springframework.boot.SpringApplication;
+
+public class Chapter24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter24Application.class, args);
+	}
+}
